673cca5f3e2c800e6909263c344c1ca225bfc0ed
lazy <3205246851@qq.com>
2024-10-04 19:39:25 +0800

    你好

---
+++ helloworld/src/com/lazy/www/helloworld.java
@@ -0,0 +1,56 @@
+package com.lazy.www;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class helloworld
+{
+    public static void main(String[] args) 
+    {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int a = rand.nextInt(100);
+        System.out.println(a);
+        while(true)
+        {
+            System.out.println("请输入： ");
+            int b = sc.nextInt();
+            if(b==a)
+            {
+                System.out.println("恭喜");
+                break;
+            }
+            else
+            {
+                System.out.println("未猜中");
+            }
+        }
+        System.out.println(a);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
